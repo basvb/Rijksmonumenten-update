897132607a1f673a13cc3677948709aefaa89a05
Bas Vb <bas.berkel@student.ru.nl>
2017-03-17 11:37:33 +0100

    add config for ODBC connection and version saving

--- read_database.py
+++ read_database.py
@@ -1,12 +1,20 @@
 import pyodbc
-db_file = r'''C:\x.mdb'''
+from ConfigParser import SafeConfigParser
+
+config = SafeConfigParser()
+config.read('data/config.ini')
+
+db_file = 'data/Extract_ODB_{version}.mdb'.format(version='V10.2.11')
 user = 'admin'
 password = ''
 
-odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb)};DBQ=%s;UID=%s;PWD=%s' %\
-                (db_file, user, password)
-# Or, for newer versions of the Access drivers:
-odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;UID=%s;PWD=%s' %\
-                (db_file, user, password)
-
+print([x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')])
+odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Wikipedia\Rijksmonumenten-update\data\Extract_ODB_V10.2.11.mdb'
 conn = pyodbc.connect(odbc_conn_str)
+cur = conn.cursor()
+
+# run a query and get the results
+SQL = 'SELECT * FROM mytable;' # your query goes here
+rows = cur.execute(SQL).fetchall()
+cur.close()
+conn.close()
