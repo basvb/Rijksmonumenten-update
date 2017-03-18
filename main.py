f6d8cc79387fcf31903f6236783bafc7bb55d58c
Bas Vb <bas.berkel@student.ru.nl>
2017-03-18 20:46:54 +0100

    As loading the data from the API will take up to an expected hour some code to safe and load the monuments locally

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ rce_db.close()
 '''
 
 wiki_db = read_database.WikipediaMonumentsDatabase()
-wiki_db.all_monuments()
+wiki_db.load_monuments_from_web()
+wiki_db.save_monuments_to_file()
--- read_database.py
+++ read_database.py
@@ -2,7 +2,16 @@ import pyodbc
 import json
 from urllib.request import urlopen
 from configparser import ConfigParser
+import pickle
 
+
+def save_obj(obj, name ):
+    with open('data/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open('data/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 class RCEMonumentsDatabase:
     def __init__(self):
@@ -24,10 +33,17 @@ class RCEMonumentsDatabase:
         self.cur.close()
         self.conn.close()
 
+
 class WikipediaMonumentsDatabase:
-    def __init__(self):
-        None
-    def all_monuments(self):
+    def __init__(self, from_file=False, filename='WikipediaMonumentsDatabase'):
+        if from_file is False:
+            self.from_file = False
+            self.monuments = None
+        else:
+            self.from_file = True
+            self.monuments = self.load_monuments_from_file(filename)
+
+    def load_monuments_from_web(self):
         #Get a list of all monuments in the database by Rijksmonumentnumber
         base_url = 'https://tools.wmflabs.org/heritage/api/api.php?action=search&srcountry=nl&srlang=nl&format=json'
         all_monuments=False
@@ -43,8 +59,20 @@ class WikipediaMonumentsDatabase:
             else:
                 break
             #Temporary break to avoid endless loops and speedtest
-            if counter==3:
+            if counter is 3:
                 break
-            counter+=1
-        print(monuments)
-        print(len(monuments))
+            counter += 1
+        self.monuments = monuments
+
+    def save_monuments_to_file(self, filename='WikipediaMonumentsDatabase'):
+        if self.monuments is not None:
+            save_obj(self.monuments, filename)
+            return True
+        else:
+            return False
+
+    def load_monuments_from_file(self, filename='WikipediaMonumentsDatabase'):
+        #TODO: test whether file exists
+        return load_obj(filename)
+
+
