2163efd5c92873ba5691dbfda50be186f8b0fb23
Bas Vb <bas.berkel@student.ru.nl>
2017-03-18 21:23:09 +0100

    functions to determine the missing monuments on Wikipedia and the monuments on Wikipedia who lost their Rijksmonument status (eg. missing in the RCE database)

--- main.py
+++ main.py
@@ -6,5 +6,26 @@ rce_db.all_monuments()
 rce_db.close()
 '''
 
-wiki_db = read_database.WikipediaMonumentsDatabase(from_file=True)
-print(wiki_db.all_monuments())
+
+def missing_monuments_on_wikipedia():
+    wiki_db = read_database.WikipediaMonumentsDatabase(from_file=True)
+    wiki_id = wiki_db.all_monuments()
+    rce_db = read_database.RCEMonumentsDatabase()
+    rce_id = rce_db.all_monuments()
+    missing_monuments = list(set(rce_id) - set(wiki_id))
+
+    rce_db.close()
+    return missing_monuments
+
+def no_longer_monument_on_wikipedia():
+    wiki_db = read_database.WikipediaMonumentsDatabase(from_file=True)
+    wiki_id = wiki_db.all_monuments()
+    rce_db = read_database.RCEMonumentsDatabase()
+    rce_id = rce_db.all_monuments()
+    missing_monuments = list(set(wiki_id) - set(rce_id))
+
+    rce_db.close()
+    return missing_monuments
+
+print(len(no_longer_monument_on_wikipedia()))
+print(len(missing_monuments_on_wikipedia()))
